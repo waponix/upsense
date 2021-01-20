cd9189ccacc67554c64d07b8c59d281ca5a51010
kevin-nicolli <kevin.nicolli@gmail.com>
2021-01-20 20:27:42 +0800

    - commit crud functions for admin

--- apollo/src/v1/resolvers/AdminResolver.ts
+++ apollo/src/v1/resolvers/AdminResolver.ts
@@ -1,6 +1,7 @@
-import { Resolver, Query, Authorized } from 'type-graphql';
+import {Resolver, Query, Mutation, Authorized, Arg} from 'type-graphql';
 import { Admin } from '../entities/Admin';
 import { getRepository, Repository } from 'typeorm';
+import {CreateAdmin, UpdateAdmin} from '../resolverInputs/AdminDataInput';
 
 @Resolver()
 export class AdminResolver
@@ -16,4 +17,43 @@ export class AdminResolver
         return this.repo.find();
     }
 
+    @Authorized('ROLE_ADMIN')
+    @Query(() => [Admin])
+    getAdmin(@Arg("id") id: number) {
+        return this.repo.findOne({ where: { id }});
+    }
+
+    @Authorized('ROLE_ADMIN')
+    @Mutation(() => Admin)
+    async createAdmin(@Arg("data") data: CreateAdmin) {
+        const admin = this.repo.create(data);
+        await admin.save();
+        return admin;
+    }
+
+    @Authorized('ROLE_ADMIN')
+    @Mutation(() => Admin)
+    async updateAdmin(@Arg("id") id: number, @Arg("data") data: UpdateAdmin) {
+        const admin = await this.repo.findOne({ where: { id }});
+
+        if (!admin) {
+            throw new Error(`The admin with id: ${id} does not exist!`);
+        }
+
+        Object.assign(admin, data);
+        await admin.save();
+
+        return admin;
+    }
+
+    @Authorized('ROLE_ADMIN')
+    @Mutation(() => Boolean)
+    async removeAdmin(@Arg("id") id: number) {
+        try {
+            await this.repo.delete(id);
+            return true;
+        } catch {
+            return false;
+        }
+    }
 }
