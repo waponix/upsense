195e692e670a3b585b9a20428d5f9f1fe001904b
kevin-nicolli <kevin.nicolli@gmail.com>
2021-01-23 19:21:08 +0800

    - arranged menu items

--- client/src/Layouts/menuItem.ts
+++ client/src/Layouts/menuItem.ts
@@ -2,164 +2,220 @@ import { MenuItemType } from '@paljs/ui/types';
 
 const items: MenuItemType[] = [
   {
-    title: 'Home',
-    icon: { name: 'home' },
-    link: { to: '/app/home/dashboard' },
+    title: 'Dashboard',
+    icon: { name: 'bar-chart-outline' },
+    link: { to: '/home/dashboard' },
   },
   {
-    title: 'FEATURES',
-    group: true,
-  },
-  {
-    title: 'Extra Components',
-    icon: { name: 'star-outline' },
-    children: [
-      {
-        title: 'Accordion',
-        link: { to: '/extra-components/accordion' },
-      },
-      {
-        title: 'Actions',
-        link: { to: '/extra-components/actions' },
-      },
-      {
-        title: 'Alert',
-        link: { to: '/extra-components/alert' },
-      },
-      {
-        title: 'List',
-        link: { to: '/extra-components/list' },
-      },
-      {
-        title: 'Spinner',
-        link: { to: '/extra-components/spinner' },
-      },
-      {
-        title: 'Progress Bar',
-        link: { to: '/extra-components/progress' },
-      },
-      {
-        title: 'Tabs',
-        link: { to: '/extra-components/tabs' },
-      },
-      {
-        title: 'Chat',
-        link: { to: '/extra-components/chat' },
-      },
-      {
-        title: 'Cards',
-        link: { to: '/extra-components/cards' },
-      },
-      {
-        title: 'Flip Card',
-        link: { to: '/extra-components/flip-card' },
-      },
-      {
-        title: 'Reveal Card',
-        link: { to: '/extra-components/reveal-card' },
-      },
-    ],
-  },
-  {
-    title: 'Forms',
-    icon: { name: 'edit-2-outline' },
-    children: [
-      {
-        title: 'Inputs',
-        link: { to: '/forms/inputs' },
-      },
-      {
-        title: 'Layout',
-        link: { to: '/forms/form-layout' },
-      },
-      {
-        title: 'Buttons',
-        link: { to: '/forms/buttons' },
-      },
-      {
-        title: 'Select',
-        link: { to: '/forms/select' },
-      },
-    ],
-  },
-  {
-    title: 'UI Features',
-    icon: { name: 'keypad-outline' },
-    children: [
-      {
-        title: 'Grid',
-        link: { to: '/ui-features/grid' },
-      },
-      {
-        title: 'Animated Searches',
-        link: { to: '/ui-features/search' },
-      },
-    ],
-  },
-  {
-    title: 'Modal & Overlays',
+    title: 'General',
     icon: { name: 'browser-outline' },
     children: [
       {
-        title: 'Popover',
-        link: { to: '/modal-overlays/popover' },
+        title: 'Reports',
+        icon: { name: 'layers-outline' },
+        link: { to: '/app/reports/index' },
       },
       {
-        title: 'Tooltip',
-        link: { to: '/modal-overlays/tooltip' },
-      },
-      {
-        title: 'Toastr',
-        link: { to: '/modal-overlays/toastr' },
+        title: 'Logs',
+        icon: { name: 'book-outline' },
+        link: { to: '/app/logs/index' },
       },
     ],
   },
   {
-    title: 'Editors',
-    icon: { name: 'text-outline' },
+    title: 'Maintenance',
+    icon: { name: 'settings-2-outline' },
     children: [
       {
-        title: 'TinyMCE',
-        link: { to: '/editors/tinymce' },
+        title: 'Admin',
+        icon: { name: 'people-outline' },
+        link: { to: '/app/admin/index' },
       },
       {
-        title: 'CKEditor',
-        link: { to: '/editors/ckeditor' },
+        title: 'Company',
+        icon: { name: 'briefcase-outline' },
+        link: { to: '/app/company/index' },
+      },
+      {
+        title: 'Branch',
+        icon: { name: 'grid-outline' },
+        link: { to: '/app/branch/index' },
+      },
+      {
+        title: 'Hubs',
+        icon: { name: 'radio-outline' },
+        link: { to: '/app/hubs/index' },
+      },
+      {
+        title: 'Zones',
+        icon: { name: 'pin-outline' },
+        link: { to: '/app/zones/index' },
       },
     ],
   },
+
   {
-    title: 'Miscellaneous',
-    icon: { name: 'shuffle-2-outline' },
+    title: 'Settings',
+    icon: { name: 'options-outline' },
     children: [
       {
-        title: '404',
-        link: { to: '/miscellaneous/404' },
+        title: 'Notification',
+        icon: { name: 'bell-outline' },
+        link: { to: '/app/notification-settings/index' },
       },
     ],
   },
-  {
-    title: 'Auth',
-    icon: { name: 'lock-outline' },
-    children: [
-      {
-        title: 'Login',
-        link: { to: '/auth/login' },
-      },
-      {
-        title: 'Register',
-        link: { to: '/auth/register' },
-      },
-      {
-        title: 'Request Password',
-        link: { to: '/auth/request-password' },
-      },
-      {
-        title: 'Reset Password',
-        link: { to: '/auth/reset-password' },
-      },
-    ],
-  },
+  // {
+  //   title: 'Extra Components',
+  //   icon: { name: 'star-outline' },
+  //   children: [
+  //     {
+  //       title: 'Accordion',
+  //       link: { to: '/extra-components/accordion' },
+  //     },
+  //     {
+  //       title: 'Actions',
+  //       link: { to: '/extra-components/actions' },
+  //     },
+  //     {
+  //       title: 'Alert',
+  //       link: { to: '/extra-components/alert' },
+  //     },
+  //     {
+  //       title: 'List',
+  //       link: { to: '/extra-components/list' },
+  //     },
+  //     {
+  //       title: 'Spinner',
+  //       link: { to: '/extra-components/spinner' },
+  //     },
+  //     {
+  //       title: 'Progress Bar',
+  //       link: { to: '/extra-components/progress' },
+  //     },
+  //     {
+  //       title: 'Tabs',
+  //       link: { to: '/extra-components/tabs' },
+  //     },
+  //     {
+  //       title: 'Chat',
+  //       link: { to: '/extra-components/chat' },
+  //     },
+  //     {
+  //       title: 'Cards',
+  //       link: { to: '/extra-components/cards' },
+  //     },
+  //     {
+  //       title: 'Flip Card',
+  //       link: { to: '/extra-components/flip-card' },
+  //     },
+  //     {
+  //       title: 'Reveal Card',
+  //       link: { to: '/extra-components/reveal-card' },
+  //     },
+  //   ],
+  // },
+
+  // {
+  //   title: 'Forms',
+  //   icon: { name: 'edit-2-outline' },
+  //   children: [
+  //     {
+  //       title: 'Inputs',
+  //       link: { to: '/forms/inputs' },
+  //     },
+  //     {
+  //       title: 'Layout',
+  //       link: { to: '/forms/form-layout' },
+  //     },
+  //     {
+  //       title: 'Buttons',
+  //       link: { to: '/forms/buttons' },
+  //     },
+  //     {
+  //       title: 'Select',
+  //       link: { to: '/forms/select' },
+  //     },
+  //   ],
+  // },
+  // {
+  //   title: 'UI Features',
+  //   icon: { name: 'keypad-outline' },
+  //   children: [
+  //     {
+  //       title: 'Grid',
+  //       link: { to: '/ui-features/grid' },
+  //     },
+  //     {
+  //       title: 'Animated Searches',
+  //       link: { to: '/ui-features/search' },
+  //     },
+  //   ],
+  // },
+  // {
+  //   title: 'Modal & Overlays',
+  //   icon: { name: 'browser-outline' },
+  //   children: [
+  //     {
+  //       title: 'Popover',
+  //       link: { to: '/modal-overlays/popover' },
+  //     },
+  //     {
+  //       title: 'Tooltip',
+  //       link: { to: '/modal-overlays/tooltip' },
+  //     },
+  //     {
+  //       title: 'Toastr',
+  //       link: { to: '/modal-overlays/toastr' },
+  //     },
+  //   ],
+  // },
+  // {
+  //   title: 'Editors',
+  //   icon: { name: 'text-outline' },
+  //   children: [
+  //     {
+  //       title: 'TinyMCE',
+  //       link: { to: '/editors/tinymce' },
+  //     },
+  //     {
+  //       title: 'CKEditor',
+  //       link: { to: '/editors/ckeditor' },
+  //     },
+  //   ],
+  // },
+  // {
+  //   title: 'Miscellaneous',
+  //   icon: { name: 'shuffle-2-outline' },
+  //   children: [
+  //     {
+  //       title: '404',
+  //       link: { to: '/miscellaneous/404' },
+  //     },
+  //   ],
+  // },
+  // {
+  //   title: 'Auth',
+  //   icon: { name: 'lock-outline' },
+  //   children: [
+  //     {
+  //       title: 'Login',
+  //       link: { to: '/auth/login' },
+  //     },
+  //     {
+  //       title: 'Register',
+  //       link: { to: '/auth/register' },
+  //     },
+  //     {
+  //       title: 'Request Password',
+  //       link: { to: '/auth/request-password' },
+  //     },
+  //     {
+  //       title: 'Reset Password',
+  //       link: { to: '/auth/reset-password' },
+  //     },
+  //   ],
+  // },
 ];
 
 export default items;
