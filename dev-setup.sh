586625ed60332d5dc76f47d2871b7ee2749dc326
Eric Reyes <eric.bermejo.reyes@gmail.com>
2021-02-07 15:32:13 +0800

    - updated configuration files - remove dev-setup.sh (build automation can also be done in docker-compose)

--- dev-setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo 'Creating build upsense-api-dev'
-docker build -t 'upsense-api-dev' -f './api/Dockerfile' '.'
-
-echo 'Creating build upsense-mqtt-broker-dev'
-docker build -t 'upsense-mqtt-broker-dev' -f './mqtt/Dockerfile' '.'
-
-echo 'Setup complete! Please run command: docker-compose up'
