4df3aed49824dfe2502cccfd44df6050f776c7f3
Eric Reyes <eric.bermejo.reyes@gmail.com>
2021-02-19 01:12:23 +0800

    - added messages collection, to be used accross the api endpoints for consistency

---
+++ api/src/messages/messages.ts
@@ -0,0 +1,47 @@
+export const OperationFailed = 'Operation failed';
+export const OperationSuccess= 'Operation successful';
+
+/**
+ * Common Messages
+ */
+export const CommonMessages = {
+    ArgumentValuesIncorrect: `${OperationFailed}, argument values incorrect`,
+    SomethingWentWrong: `${OperationFailed}, something went wrong`
+}
+
+/**
+ * Auth Messages
+ */
+export const AuthMessages = {
+    InvalidCredentials: `${OperationFailed}, invalid credentials`,
+}
+
+/**
+ * Manager Messages
+ */
+export const AdminMessages = {
+    NotFound: `${OperationFailed}, unable to find Admin data`,
+    UnableToSave: `${OperationFailed}, unable to save Admin data`,
+    UnableToUpdate: `${OperationFailed}, unable to update Admin data`,
+    UnableToDelete: `${OperationFailed}, unable to delete Admin data`,
+}
+
+/**
+ * Manager Messages
+ */
+export const ManagerMessages = {
+    NotFound: `${OperationFailed}, unable to find Manager data`,
+    UnableToSave: `${OperationFailed}, unable to save Manager data`,
+    UnableToUpdate: `${OperationFailed}, unable to update Manager data`,
+    UnableToDelete: `${OperationFailed}, unable to delete Manager data`,
+}
+
+/**
+ * User Messages
+ */
+export const UserMessages = {
+    NotFound: `${OperationFailed}, unable to find User data`,
+    UnableToSave: `${OperationFailed}, unable to save User data`,
+    UnableToUpdate: `${OperationFailed}, unable to update User data`,
+    UnableToDelete: `${OperationFailed}, unable to delete User data`,
+}
